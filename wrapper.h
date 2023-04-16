765b28b5ae9baa1062e98cf60cc8d31d7a84d403
Mischa Spiegelmock <me@mish.dev>
2023-04-16 13:53:02 -0700

    Bindgen and build bundled  (#1)
    * Use bindgen and pkg-config
    
    * use SDL3 headers, use git submodule for including libsdl3

---
+++ wrapper.h
@@ -0,0 +1,13 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_syswm.h>
+#include <SDL3/SDL_vulkan.h>
+
+/**
+ * <div rustbindgen replaces="VkInstance"></div>
+ */
+typedef uintptr_t VkInstance_int;
+
+/**
+ * <div rustbindgen replaces="VkSurfaceKHR"></div>
+ */
+typedef uint64_t VkSurfaceKHR_int;
